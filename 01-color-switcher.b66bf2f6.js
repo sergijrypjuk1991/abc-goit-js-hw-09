a658bb0c74c535318ec73dfeecfdefe993a68eb4
sergijrypjuk1991 <101973508+sergijrypjuk1991@users.noreply.github.com>
2022-10-23 05:28:35 +0000

    Deploying to gh-pages from @ sergijrypjuk1991/abc-goit-js-hw-09@5623b1b2a15299b033ab3be6b099b934b6f667e3 🚀

---
+++ 01-color-switcher.b66bf2f6.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};let e=null;t.stopBtn.disabled=!0,t.startBtn.addEventListener("click",(n=>{e=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,t.stopBtn.disabled=!1,t.startBtn.disabled=!0}),1e3)})),t.stopBtn.addEventListener("click",(()=>{clearInterval(e),t.stopBtn.disabled=!0,t.startBtn.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.b66bf2f6.js.map
